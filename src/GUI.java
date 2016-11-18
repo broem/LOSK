a6d3cac1bf6dd144a40fcc9c03c57533b02c6e19
kimpflerze <kimpflerze@vcu.edu>
2016-11-17 21:05:06 -0500

    gui set up, still needs to be tied into rest of program

--- src/GUI.java
+++ src/GUI.java
@@ -1,10 +1,15 @@
+import java.awt.*;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
-import java.awt.Color;
-import java.awt.ScrollPane;
+import javax.swing.border.Border;
 
 public class GUI extends JFrame {
+
+    private static JTextArea logArea;
+    private static JTextArea dataArea;
 
     /**
      * Create the application.
@@ -18,29 +23,67 @@ public class GUI extends JFrame {
      */
     private void initialize() {
         setTitle("LOSK");
-        setForeground(Color.BLACK);
-        setBackground(Color.BLACK);
-        getContentPane().setBackground(Color.BLACK);
-        setBounds(100, 100, 450, 300);
+        setBounds(100, 100, 625, 365);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        //frame.setUndecorated(true);
         getContentPane().setLayout(null);
-        
-        JTextArea txtpnOk = new JTextArea();
-//        txtpnOk.setEditable(true);
-        txtpnOk.setLineWrap(true);
-        txtpnOk.setEditable(false);
-        txtpnOk.setForeground(Color.GREEN);
-        txtpnOk.setBackground(Color.BLACK);
-        txtpnOk.setBounds(20, 11, 382, 239);
-        txtpnOk.append("LOSK:");
-        
-        txtpnOk.setEditable(true);
-        
-        getContentPane().add(txtpnOk);
-        
+
+        Border border = BorderFactory.createLineBorder(Color.BLACK);
+
+        //Text field for input
+        JTextField inputField = new JTextField("Terminal Input Here");
+        inputField.setEditable(true);
+        inputField.setBounds(5, 5, 300, 25);
+        inputField.setBorder(border);
+
+        //Text area for log
+        logArea = new JTextArea();
+        logArea.setEditable(false);
+
+        JScrollPane logAreaScrollPane = new JScrollPane(logArea);
+        logAreaScrollPane.setBounds(5, 40, 300, 300);
+        logAreaScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        logAreaScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        logAreaScrollPane.setBorder(border);
+
+        //Text area for info on processes
+        dataArea = new JTextArea("Processes Data Here");
+        dataArea.setEditable(false);
+
+        JScrollPane dataAreaScrollPane = new JScrollPane(dataArea);
+        dataAreaScrollPane.setBounds(320, 5, 300, 335);
+        dataAreaScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        dataAreaScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        dataAreaScrollPane.setBorder(border);
+
+        getContentPane().add(inputField);
+        getContentPane().add(logAreaScrollPane);
+        getContentPane().add(dataAreaScrollPane);
+
+        inputField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                logArea.append(inputField.getText()+"\n");
+                inputField.setText("");
+            }
+        });
     }
-    
+
+    public void appendLogArea(String string){
+        logArea.append(string);
+    }
+
+    public void clearLogArea(){
+        logArea.setText("");
+    }
+
+    public void appendDataArea(String string){
+        dataArea.append(string);
+    }
+
+    public void clearDataArea(){
+        dataArea.setText("");
+    }
+
     public void onEnter(){
         
     }
